beb374d1bad765b37f07d11aaf54451f32aed01f
HollyLiang <lianghonglian521@163.com>
2023-07-27 15:34:07 +0800

    添加自定义日志内容

--- spring6-first/src/main/java/com/hzaucoi/spring6/TestUser.java
+++ spring6-first/src/main/java/com/hzaucoi/spring6/TestUser.java
@@ -1,10 +1,14 @@
 package com.hzaucoi.spring6;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.slf4j.Logger;
 public class TestUser {
+    //创建日志对象
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
     @Test
     public void testAdd() {
         // 1. 加载spring配置文件，根据创建对象
@@ -13,5 +17,7 @@ public class TestUser {
         User user = (User) context.getBean("user");
         // 3. 调用方法
         user.add();
+        // 打印自定义日志
+        logger.info("执行成功");
     }
 }
