85bad542486596e9aded956bf756626b853a5679
HollyLiang <lianghonglian521@163.com>
2023-08-03 11:11:01 +0800

    junit5测试

---
+++ spring6-junit/src/main/java/com/hazucoi/spring6/bean/TestUser.java
@@ -0,0 +1,22 @@
+package com.hazucoi.spring6.bean;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+//两种方式均可
+//方式一
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:bean.xml")
+//方式二
+//@SpringJUnitConfig(locations = "classpath:bean.xml")
+public class TestUser {
+    @Autowired
+    private User user;
+    @Test
+    public void testUser(){
+        System.out.println(user);
+    }
+}
---
+++ spring6-junit/src/main/java/com/hazucoi/spring6/bean/User.java
@@ -0,0 +1,10 @@
+package com.hazucoi.spring6.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    public User() {
+        System.out.println("run user");
+    }
+}
