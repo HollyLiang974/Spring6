a9f8660400cf5dc8ead0ea8c7e9562175ae96a48
HollyLiang <lianghonglian521@163.com>
2023-08-03 11:31:01 +0800

    junit5测试

--- spring6-junit/src/main/java/com/hazucoi/spring6/bean/TestUserJunit5.java
+++ spring6-junit/src/main/java/com/hazucoi/spring6/bean/TestUserJunit5.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
 //两种方式均可
 //方式一
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:bean.xml")
 //方式二
 //@SpringJUnitConfig(locations = "classpath:bean.xml")
-public class TestUser {
+public class TestUserJunit5 {
     @Autowired
     private User user;
     @Test
