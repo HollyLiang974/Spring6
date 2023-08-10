c474a5fe76d8debeb57e7a234b648ae21f059c8f
HollyLiang <lianghonglian521@163.com>
2023-08-10 17:54:55 +0800

    classpath通配符使用

--- spring6-resources/src/main/java/com/hzaucoi/spring6/context/classpathDemo.java
+++ spring6-resources/src/main/java/com/hzaucoi/spring6/context/classpathDemo.java
@@ -13,8 +13,14 @@ public class classpathDemo {
          * 但通过指定classpath:前缀强制搜索类加载路径
          * classpath:bean.xml
          * */
-        ApplicationContext ctx =
-                new ClassPathXmlApplicationContext("classpath:bean.xml");
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:bean.xml");
+
+//        classpath * :前缀提供了加载多个XML配置文件的能力，当使用classpath*:前缀来指定XML配置文件时，
+//        系统将搜索类加载路径，找到所有与文件名匹配的文件，分别加载文件中的配置定义，最后合并成一个ApplicationContext。
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:bean.xml");
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:bean*.xml");
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:bean*.xml");
+
         System.out.println(ctx);
         Resource resource = ctx.getResource("hzaucoi.txt");
         System.out.println(resource.getFilename());
