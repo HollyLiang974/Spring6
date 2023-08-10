3dec052f07aee9f7e633f1d5b51d6bbf43a683fb
HollyLiang <lianghonglian521@163.com>
2023-08-10 18:24:59 +0800

    使用Spring6国际化

---
+++ spring6-i18n/src/main/java/com/hazucoi/spring6/javai18n/Demo2.java
@@ -0,0 +1,23 @@
+package com.hazucoi.spring6.javai18n;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.Date;
+import java.util.Locale;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        //传递动态参数，使用数组形式对应{0} {1}顺序
+        Object[] objs = new Object[]{"atguigu",new Date().toString()};
+
+        //www.atguigu.com为资源文件的key值,
+        //objs为资源文件value值所需要的参数,Local.CHINA为国际化为语言
+        String str=context.getMessage("www.atguigu.com", objs, Locale.CHINA);
+        System.out.println(str);
+    }
+}
